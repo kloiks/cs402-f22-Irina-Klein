122a3cf915f02023bd3957d35a35e3cbf0b0a6ce
kloiks <92125925+kloiks@users.noreply.github.com>
2022-09-12 15:53:12 -0500

    Add h1 ex 2 code files
    Multiplying matrices and measuring run time

---
+++ matMultDouble_a.py
@@ -0,0 +1,48 @@
+import timeit
+import numpy as np 
+from time import process_time
+
+
+def matMultDouble():
+    
+    R1 = 100
+    C1 = 200
+    R2 = C1
+    C2 = R1
+
+    mat1 = np.random.rand(R1, C1) 
+    mat2 = np.random.rand(R2, C2)
+
+    rslt = np.zeros((R1, R1), dtype=float)
+    
+    for i in range(0, R1):
+        for j in range(0, C2):
+            for k in range(0, R2):
+                rslt[i][j] += mat1[i][k] * mat2[k][j]
+    
+
+time_ex_d = [timeit.timeit(f'matMultDouble()',
+               number=100,
+               globals=globals())]
+
+
+time_CPU_d = 0
+for i in range(0,100):
+    t_st = process_time() 
+    
+    matMultDouble()
+    
+    t_end = process_time() 
+    
+    time_CPU_d += t_end - t_st 
+
+
+
+print("Execution time of matMultDouble part a: ",time_ex_d)
+print("CPU time of matMultDouble part a: ",time_CPU_d)
+
+
+
+
+
+
---
+++ matMultInt_a.py
@@ -0,0 +1,48 @@
+import timeit
+import numpy as np 
+from time import process_time
+
+
+def matMultInt():
+    
+    R1 = 100
+    C1 = 200
+    R2 = C1
+    C2 = R1
+
+    mat1 = np.random.randint(-10, high=10, size=(R1, C1), dtype='l')
+    mat2 = np.random.randint(-10, high=10, size=(R2, C2), dtype='l')
+
+    rslt = np.zeros((R1, R1), dtype=int)
+    
+    
+    for i in range(0, R1):
+        for j in range(0, C2):
+            for k in range(0, R2):
+                rslt[i][j] += mat1[i][k] * mat2[k][j]
+
+
+
+time_ex_int = [timeit.timeit(f'matMultInt()',
+               number=100,
+               globals=globals())]
+
+
+time_CPU_int = 0
+for i in range(0,100):
+    t_st = process_time() 
+    
+    matMultInt()
+    
+    t_end = process_time() 
+    
+    time_CPU_int += t_end - t_st 
+
+print("Execution time of matMultInt part a: ",time_ex_int)
+print("CPU time of matMultInt part a: ",time_CPU_int)
+
+
+
+
+
+
